e959ef14277957a222292364e584ed02e78d82f3
fatima-s10 <fatimasaboor05@gmail.com>
2024-09-15 11:27:47 +0500

    Add files via upload

---
+++ gun.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ system ("color 09");
+ cout << " +--^----------,--------,-----,--------^-, " << endl;
+ cout << "  |  ||||||||   '--------'     |          o " << endl;
+ cout << "  '+---------------------------^----------| " <<endl;
+ cout << "    ',---------,---------,--------------' " << endl;
+ cout << "      / XXXXXX  /'|       /' " << endl;
+ cout << "     / XXXXXX  /  |      /'   " << endl;
+ cout << "    / XXXXXX  /'-------'   " << endl;
+ cout << "   / XXXXXX  / " << endl;
+ cout << "  / XXXXXX  /  " << endl;
+ cout << " (_________( " << endl;
+ cout << "  '-------'" << endl;
+}
---
+++ hangmanimg.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ system ("color 0E");
+ cout << "              ______     " << endl;
+ cout << "             |      |     " << endl;
+ cout << "             |      |     " << endl;
+ cout << "             |      O     " << endl;
+ cout << "             |    //|\\   " << endl;
+ cout << "             |    // \\   " << endl;
+ cout << "             |            " << endl;
+ cout << "            _|_________            " << endl;
+ cout << "           |           |__           " << endl;
+ cout << "           |              |__     " << endl;
+ cout << "           |_________________|     " << endl;
+}
